b3ca6bf556ec6e981cd40d82bbe436daf3c6c09b
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-02-25 11:34:34 +0200

    add new state in context selectedElement

--- src/context/DesignerContext.tsx
+++ src/context/DesignerContext.tsx
@@ -1,18 +1,23 @@
 'use client';
 
 import { FormElementInstance } from '@/components/form-builder/designer/FormElements';
-import { createContext, PropsWithChildren, useState } from 'react';
+import { createContext, Dispatch, PropsWithChildren, SetStateAction, useState } from 'react';
 
 type DesignerContextType = {
     elements: FormElementInstance[];
     addElement: (index: number, element: FormElementInstance) => void;
     removeElement: (id: string) => void;
+
+    selectedElement: FormElementInstance | null;
+    setSelectedElement:  Dispatch<SetStateAction<FormElementInstance | null>>;
 }
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
 
 export default function DesignerContextProvider({children}: PropsWithChildren) {
     const [elements, setElements] = useState<FormElementInstance[]>([]);
+    const [selectedElement, setSelectedElement] = useState<FormElementInstance | null>(null);
+
     const addElement = (index: number, element: FormElementInstance) => {
         setElements((prevElements) => {
             const newElements = [...prevElements];
@@ -29,7 +34,10 @@ export default function DesignerContextProvider({children}: PropsWithChildren) {
     const value = {
         elements,
         addElement,
-        removeElement
+        removeElement,
+
+        selectedElement,
+        setSelectedElement,
     }
 
     return (
