f951f8154de9b67ef857923d5accf0e812c2a72c
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-02-25 15:13:04 +0200

    change import for AlertDialogComponents

--- src/components/form-builder/buttons/PublishFormBtn.tsx
+++ src/components/form-builder/buttons/PublishFormBtn.tsx
@@ -3,17 +3,7 @@ import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { FaSpinner } from "react-icons/fa";
 import { MdOutlinePublish } from "react-icons/md";
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "../../ui/alert-dialog";
+import * as AlertDialogComponents from "../../ui/alert-dialog";
 import { Button } from "../../ui/button";
 import { toast } from "../../ui/use-toast";
 
@@ -38,28 +28,32 @@ function PublishFormBtn({ id }: { id: number }) {
     }
 
     return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>
+        <AlertDialogComponents.AlertDialog>
+            <AlertDialogComponents.AlertDialogTrigger asChild>
                 <Button className="gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400">
                     <MdOutlinePublish className="h-4 w-4" />
                     Publish
                 </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
+            </AlertDialogComponents.AlertDialogTrigger>
+            <AlertDialogComponents.AlertDialogContent>
+                <AlertDialogComponents.AlertDialogHeader>
+                    <AlertDialogComponents.AlertDialogTitle>
+                        Are you absolutely sure?
+                    </AlertDialogComponents.AlertDialogTitle>
+                    <AlertDialogComponents.AlertDialogDescription>
                         This action cannot be undone. After publishing you will not be able to edit this form. <br />
                         <br />
                         <span className="font-medium">
               By publishing this form you will make it available to the public and you will be able to collect
               submissions.
             </span>
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction
+                    </AlertDialogComponents.AlertDialogDescription>
+                </AlertDialogComponents.AlertDialogHeader>
+                <AlertDialogComponents.AlertDialogFooter>
+                    <AlertDialogComponents.AlertDialogCancel>
+                        Cancel
+                    </AlertDialogComponents.AlertDialogCancel>
+                    <AlertDialogComponents.AlertDialogAction
                         disabled={loading}
                         onClick={(e) => {
                             e.preventDefault();
@@ -67,10 +61,10 @@ function PublishFormBtn({ id }: { id: number }) {
                         }}
                     >
                         Proceed {loading && <FaSpinner className="animate-spin" />}
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+                    </AlertDialogComponents.AlertDialogAction>
+                </AlertDialogComponents.AlertDialogFooter>
+            </AlertDialogComponents.AlertDialogContent>
+        </AlertDialogComponents.AlertDialog>
     );
 }
 
