a8d0d201b66bf57e59f5ec2a586c00473b62c6a9
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-02-25 22:59:56 +0200

    add submit page

---
+++ src/app/submit/[formUrl]/page.tsx
@@ -0,0 +1,21 @@
+import { GetFormContentByUrl } from '../../../actions/form';
+import { FormElementInstance } from '../../../components/form-builder/designer/FormElements';
+
+type SubmitPageProps = {
+    params: {
+        formUrl: string;
+    }
+};
+
+async function SubmitPage({params}: SubmitPageProps) {
+    const form = await GetFormContentByUrl(params.formUrl);
+    if (!form) {
+        throw new Error('Form not found');
+    }
+
+    const formContent = JSON.parse(form.content) as FormElementInstance[];
+
+    return <div>{formContent.map((f) => f.type)}</div>;
+}
+
+export default SubmitPage;
