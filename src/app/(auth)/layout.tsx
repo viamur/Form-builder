494b0164e417384920804e66fd05ae93f8a497a2
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-03-02 10:55:08 +0200

    add AuthLayout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+export default function AuthLayout({children}: PropsWithChildren) {
+    return (
+        <main className="w-screen h-screen flex items-center justify-center content-center bg-accent">
+            {children}
+        </main>
+    )
+}
