63778cb494068816906e5e52af8f852437aacebe
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-03-09 13:12:41 +0200

    update declare global for env variable

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,20 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+            CLERK_SECRET_KEY: string;
+            NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+            NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+            NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string;
+            NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string;
+            POSTGRES_PRISMA_URL: string;
+            POSTGRES_URL_NON_POOLING: string;
+        }
+    }
+}
+
+export {};
